e31fc7c08396794f534e82688e4ef741cf64cdac
Piotr Brzezinski <piotrbrzezinski+github@outlook.com>
2015-05-19 22:57:39 +0200

    Composite pattern

---
+++ DesignPatterns/Structural/Composite/Composite.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Composite
+{
+    public class Composite : IComponent
+    {
+        private List<IComponent> _childs;
+
+        public Composite(params IComponent[] components)
+        {
+            _childs = new List<IComponent>();
+            _childs.AddRange(components);
+        }
+
+        public void Add(IComponent component)
+        {
+            _childs.Add(component);
+        }
+        
+        public void AddRange(params IComponent[] components)
+        {
+            _childs.AddRange(components);
+        }
+
+        public void Delete(IComponent component)
+        {
+            _childs.Remove(component);
+        }
+
+        public void Operation()
+        {
+            foreach (IComponent cmp in _childs)
+            {
+                cmp.Operation();
+            }
+        }
+    }
+}
---
+++ UnitTests/Structural/Composite/CompositeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.Structural.Composite;
+
+namespace UnitTests.Structural.Composite
+{
+    [TestClass]
+    public class CompositeTest
+    {
+        [TestMethod]
+        public void CompositeUsage()
+        {
+            IComponent c1 = new Leaf();
+            IComponent c2 = new Leaf();
+            IComponent c3 = new Leaf();
+
+            DesignPatterns.Structural.Composite.Composite composite = new DesignPatterns.Structural.Composite.Composite(c1, c2);
+            composite.Add(c3);
+
+            composite.Operation();
+
+            Assert.IsTrue(true);
+        }
+    }
+}
