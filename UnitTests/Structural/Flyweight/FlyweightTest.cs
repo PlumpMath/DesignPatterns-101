91367582a1d1b329af79abf5ad940e850d670bad
Piotr Brzezinski <piotrbrzezinski+github@outlook.com>
2015-05-26 22:42:43 +0200

    Flyweight pattern

---
+++ UnitTests/Structural/Flyweight/FlyweightTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.Structural.Flyweight;
+
+namespace UnitTests.Structural.Flyweight
+{
+    [TestClass]
+    public class FlyweightTest
+    {
+        [TestMethod]
+        public void FlyweightUsage()
+        {
+            string document = "AAZZBBZB";
+            char[] chars = document.ToCharArray();
+
+            CharacterFactory factory = new CharacterFactory();
+
+            int pointSize = 10;
+
+            foreach (char c in chars)
+            {
+                pointSize++;
+                Character character = factory.GetCharacter(c);
+                character.Display(pointSize);
+            }
+
+            Assert.IsTrue(true);
+        }
+    }
+}
